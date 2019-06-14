64f1705c01be6473b7dc91d1dec91b58d11fe329
pacosta1982 <acosta.aleoli@gmail.com>
2019-06-14 07:48:29 -0400

    Ajustes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -472,7 +472,7 @@ allNextBtn.click(function () {
     var curStep = $(this).closest(".setup-content"),
         curStepBtn = curStep.attr("id"),
         nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-        curInputs = curStep.find("input[type='text'],input[type='url'],input[type='radio']"),
+        curInputs = curStep.find("input[type='text'],input[type='url'],input[type='radio'],select"),
         isValid = true;
 
     $(".form-group").removeClass("has-error");
